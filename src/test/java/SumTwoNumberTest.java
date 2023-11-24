a096b7ef0b332d378a32531b26aecedfdf69880f
HansXU <Hanchuan.xu@outlook.com>
2023-11-24 20:14:35 +0800

    ddddd

--- src/test/java/SumTwoNumberTest.java
+++ src/test/java/SumTwoNumberTest.java
@@ -4,7 +4,7 @@ public class SumTwoNumberTest {
     @Test
     public void testTwoSum(){
         SumTwoNumber sumTwoNumber = new SumTwoNumber();
-        Assert.assertEquals("Hello",3,sumTwoNumber.sum(1,2));
+        Assert.assertEquals("Hello",4,sumTwoNumber.sum(1,2));
 //        int[] nums1 = new int[]{2, 7, 11, 15};
 //        int target1 = 9;
 //        Assert.assertArrayEquals(new int[]{2,1},sumTwoNumber.twoSum(nums1,target1));
